d435d22ee916fda2904e436c914b0ab815a8e4f9
Kait12woodard <kwoodard22@mail.wou.edu>
2025-02-20 11:13:45 -0800

    Added first test and created view that will be needed to pass test in next commit

--- MoviesMadeEasyProject/MoviesMadeEasy/Controllers/UserController.cs
+++ MoviesMadeEasyProject/MoviesMadeEasy/Controllers/UserController.cs
@@ -1,22 +1,25 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MoviesMadeEasy.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace MoviesMadeEasy.Controllers;
 
 public class UserController : Controller
 {
     private readonly ILogger<UserController> _logger;
-
-    public UserController(ILogger<UserController> logger)
+    private readonly SignInManager<IdentityUser> _signInManager;
+    public UserController(ILogger<UserController> logger, SignInManager<IdentityUser> signInManager)
     {
         _logger = logger;
+        _signInManager = signInManager;
     }
 
-    public IActionResult Login()
+    public async Task<IActionResult> Login(string email, string password, string returnUrl)
     {
-        return View();
+        return View(); // Temporary placeholder to make the test fail properly
     }
+
 
     public IActionResult Register()
     {
