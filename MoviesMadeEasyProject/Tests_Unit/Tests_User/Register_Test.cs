1e7aae542e0f84b7c6cd16eb0dcd48538319b72b
kiraryder <kmorgan22@mail.wou.edu>
2025-02-23 12:09:55 -0800

    Unit tests started, code adjustments

---
+++ MoviesMadeEasyProject/Tests_Unit/Tests_User/Register_Test.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using MoviesMadeEasy.Controllers;
+using MoviesMadeEasy.DAL.Abstract;
+using MoviesMadeEasy.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace MME_Tests
+{
+    [TestFixture]
+    public class RegisterTests
+    {
+
+    }
+
+}
